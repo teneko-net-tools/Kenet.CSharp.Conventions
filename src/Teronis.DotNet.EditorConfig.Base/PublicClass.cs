bba198ca8524c8e52c2e3aff2243d8067a0bd317
teneko <giteroneko@gmail.com>
2022-11-06 03:09:09 +0000

    fix: reorganized, refactored and enhanced EditorConfig generation

---
+++ src/Teronis.DotNet.EditorConfig.Base/PublicClass.cs
@@ -0,0 +1,22 @@
+﻿namespace Teronis.DotNet.EditorConfig.Base
+{
+    // The disabling of such warnings is the assertion
+#pragma warning disable RCS1138 // Add summary to documentation comment.
+#pragma warning disable CS8618 // Non-nullable field must contain a non-null value when exiting constructor. Consider declaring as nullable.
+#pragma warning disable CS1591 // Missing XML comment for publicly visible type or member
+    /// <summary>
+    ///
+    /// </summary>
+    /// <typeparam name="TSomething"></typeparam>
+    public class PublicClass<TSomething>
+    {
+        public PublicClass(TSomething _)
+        {
+        }
+
+        public string Test { get; }
+    }
+#pragma warning restore CS1591 // Missing XML comment for publicly visible type or member
+#pragma warning restore CS8618 // Non-nullable field must contain a non-null value when exiting constructor. Consider declaring as nullable.
+#pragma warning restore RCS1138 // Add summary to documentation comment.
+}
---
+++ src/Teronis.DotNet.EditorConfig.Test/PublicClass.cs
@@ -0,0 +1,15 @@
+﻿namespace Teronis.DotNet.EditorConfig.Base
+{
+    /// <summary>
+    ///
+    /// </summary>
+    /// <typeparam name="TSomething"></typeparam>
+    public class PublicClass<TSomething>
+    {
+        public PublicClass(TSomething _)
+        {
+        }
+
+        public string Test { get; } = null!;
+    }
+}
